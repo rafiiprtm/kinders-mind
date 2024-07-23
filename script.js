440923736c5a4ef7e1d00b2f3b44f91dcb6d584e
unknown <you@example.com>
2024-07-23 18:32:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
